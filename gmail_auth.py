3c264b45010eb2c4d5c21acae14e01be8c20e5c3
BRIAN-ALIENWARE\Brian Lin <brianlin.0222@gmail.com>
2021-02-07 00:56:30 -0500

    Gmail authentication method update

--- gmail_auth.py
+++ gmail_auth.py
@@ -7,7 +7,7 @@ from google.auth.transport.requests import Request
 
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+SCOPES = ['https://www.googleapis.com/auth/gmail.labels']
 
 
 def main():
