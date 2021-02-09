41c75d30f8c5643f148a735463d1ef6c2edfb574
BRIAN-ALIENWARE\Brian Lin <brianlin.0222@gmail.com>
2021-02-08 20:57:21 -0500

    Updated color map logic to display price gain and loss correctly in treemap

--- charts.py
+++ charts.py
@@ -7,9 +7,17 @@ import squarify as sq
 class treemap():
     
     def gr_colors(series):
-        theme = matplotlib.cm.bwr_r
-        cmap = matplotlib.colors.Normalize(vmin=min(series),vmax=max(series))
-        return [theme(cmap(v)) for v in series]
+        #this will not generate a correct treemap when there is an outlier in the portfolio
+        #(a stock either greatly outperform or underperform compare to its peers)
+        # theme = matplotlib.cm.bwr_r
+        
+        gtheme = matplotlib.cm.Blues
+        rtheme = matplotlib.cm.autumn
+        
+        g_cmap = matplotlib.colors.Normalize(vmin=0,vmax=max(series))
+        r_cmap = matplotlib.colors.Normalize(vmin=min(series),vmax=0)
+        rgb_vec = [gtheme(g_cmap(v)) if v>0 else rtheme(r_cmap(v)) for v in series]
+        return rgb_vec
     
     
     def draw(size_scale,label,color_scale):
